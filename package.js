9f34d08b04af04752e581e6f0f76ca17af02afd1
plb666 <pbeato@kubix.it>
2024-01-24 15:44:53 +0100

    Migration 3.0

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
-  name: 'universe:i18n',
-  version: '3.0.0',
-  summary:
-    'Lightweight i18n, YAML & JSON translation files, string interpolation, incremental & remote loading',
-  git: 'https://github.com/vazco/meteor-universe-i18n.git',
-  documentation: './ATMOSPHERE.md',
+    name: 'universe:i18n',
+    version: '3.0.0',
+    summary:
+        'Lightweight i18n, YAML & JSON translation files, string interpolation, incremental & remote loading',
+    git: 'https://github.com/vazco/meteor-universe-i18n.git',
+    documentation: './ATMOSPHERE.md',
 });
 
 const npmDependencies = {
-  'js-yaml': '4.1.0',
-  'strip-json-comments': '3.1.1',
+    'js-yaml': '4.1.0',
+    'strip-json-comments': '3.1.1',
 };
 
 Npm.depends(npmDependencies);
@@ -39,7 +39,30 @@ Package.onUse(function (api) {
     'zodern:types@1.0.13',
   ]);
 
-  api.mainModule('source/client.ts', 'client');
-  api.mainModule('source/server.ts', 'server');
-  api.export(['i18n', '_i18n']);
+    api.mainModule('source/client.ts', 'client');
+    api.mainModule('source/server.ts', 'server');
+    api.export(['i18n', '_i18n']);
+});
+
+Package.onTest(function (api) {
+    api.use([
+        'meteortesting:mocha',
+        'practicalmeteor:chai',
+        'practicalmeteor:sinon',
+        'typescript',
+        'universe:i18n',
+    ]);
+
+    api.addFiles([
+        'tests/data/de/ch.i18n.yml',
+        'tests/data/en_gb.i18n.json',
+        'tests/data/es-es.i18n.json',
+        'tests/data/fr.i18n.yml',
+        'tests/data/it-it.i18n.yml',
+        'tests/data/nest/el/y.i18n.yml',
+        'tests/data/pl-pl/x.i18n.yml',
+        'tests/data/z.i18n.yml',
+    ]);
+    api.addFiles(['tests/client.ts', 'tests/common.ts'], 'client');
+    api.addFiles(['tests/server.ts', 'tests/common.ts'], 'server');
 });
